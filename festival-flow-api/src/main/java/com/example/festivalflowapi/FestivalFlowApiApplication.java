347e3502120dc5b14142ed31e86104c0bc13bf3e
Navodya <nawodhyasrilanka2015@gmail.com>
2026-08-05 21:26:49 +0530

    chore: initial spring boot project

---
+++ festival-flow-api/src/main/java/com/example/festivalflowapi/FestivalFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.festivalflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalFlowApiApplication.class, args);
+	}
+
+}
---
+++ festival-flow-api/src/test/java/com/example/festivalflowapi/FestivalFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.festivalflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
