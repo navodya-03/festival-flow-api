2fbb44bb0135f279f500ea722a4dd705fba9e685
Navodya <nawodhyasrilanka2015@gmail.com>
2026-08-07 12:13:18 +0530

    refactor:add getters and setters to Festival model

--- festival-flow-api/src/main/java/com/example/festivalflowapi/model/Festival.java
+++ festival-flow-api/src/main/java/com/example/festivalflowapi/model/Festival.java
@@ -26,6 +26,18 @@ public class Festival {
     @OneToMany(mappedBy = "festival")
     List <Stage> stages = new ArrayList<>();
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLocation() {
+        return location;
+    }
 
 
     
