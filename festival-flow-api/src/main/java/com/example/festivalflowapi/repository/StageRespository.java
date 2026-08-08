08226548948fc23e30bb85b2cc4b8cbede0ed51a
Navodya <nawodhyasrilanka2015@gmail.com>
2026-08-08 08:28:01 +0530

    fix: updated other files to match the data type of model pk

--- festival-flow-api/src/main/java/com/example/festivalflowapi/repository/StageRespository.java
+++ festival-flow-api/src/main/java/com/example/festivalflowapi/repository/StageRespository.java
@@ -1,4 +1,4 @@
-package com.example.repository;
+package com.example.festivalflowapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.example.festivalflowapi.model.Stage;
 
 @Repository
-public interface StageRespository extends JpaRepository<Stage, Long> {
+public interface StageRespository extends JpaRepository<Stage, Integer> {
 
     
 } 
